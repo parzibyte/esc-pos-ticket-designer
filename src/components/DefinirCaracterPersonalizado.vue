6572d959ebcd6007851596298c32c6dc8c5f3033
Luis Cabrera Benito <parzibyte@gmail.com>
2023-09-21 20:55:55 -0600

    Limpieza de código y funcionando en móviles

--- src/components/DefinirCaracterPersonalizado.vue
+++ src/components/DefinirCaracterPersonalizado.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, onMounted, ref, type Ref } from "vue"
+import { onMounted, ref, type Ref } from "vue"
 import Brush from 'vue-material-design-icons/Brush.vue';
 import Eraser from 'vue-material-design-icons/Eraser.vue';
 import Broom from 'vue-material-design-icons/Broom.vue';
@@ -18,7 +18,6 @@ type Boton = {
 }
 let isDrawing = false;
 let matriz: Ref<string> = ref("");
-
 const accionSeleccionada: Ref<Accion> = ref(Accion.Pintar);
 const botones: Ref<Array<Boton>> = ref([
     {
@@ -44,51 +43,61 @@ const botones: Ref<Array<Boton>> = ref([
         accion: Accion.Limpiar,
     },
 ]);
-const canvas = ref(null);
+const canvas: Ref<HTMLCanvasElement | null> = ref(null);
+let verdaderoCanvas: HTMLCanvasElement;
+let contexto: CanvasRenderingContext2D;
 const BIT_ENCENDIDO = "1",
-    BIT_APAGADO = "0";
-let arregloDeCaracter = Array(24).fill(false).map(() => Array(12).fill(BIT_APAGADO))
-function getCursorPosition(canvas, event: any) {
-    const rect = canvas.value.getBoundingClientRect()
-    const x = event.clientX - rect.left
-    const y = event.clientY - rect.top
+    BIT_APAGADO = "0",
+    MEDIDA_CUADRO = 10,
+    COLOR_NEGRO = "black",
+    COLOR_BLANCO = "white",
+    COLOR_GRIS = "#e2e8f0";
+let arregloDeCaracter: Array<Array<string>>;
+
+function getCursorPosition(event: PointerEvent | TouchEvent | MouseEvent) {
+    const rect = verdaderoCanvas.getBoundingClientRect();
+    let x: number = 0, y: number = 0;
+    if (event instanceof PointerEvent || event instanceof MouseEvent) {
+        x = event.clientX - rect.left;
+        y = event.clientY - rect.top;
+    } else if (event instanceof TouchEvent) {
+        if (event.touches.length > 0) {
+            x = event.touches[0].clientX - rect.left;
+            y = event.touches[0].clientY - rect.top;
+        }
+    }
     return [x, y];
 }
 
-const convertirArregloACadenaParaCaracter = arreglo => {
+const convertirArregloACadenaParaCaracter = (arreglo: Array<Array<string>>) => {
     return arreglo.map(fila => fila.join("")).join("\n");
 }
 
-const MEDIDA_CUADRO = 10;
 const limpiarCanvas = () => {
-    arregloDeCaracter = arregloDeCaracter.map(fila => {
-        return fila.map(cadena => BIT_APAGADO);
-    });
+    arregloDeCaracter = Array(24).fill(false).map(() => Array(12).fill(BIT_APAGADO))
     dibujar();
 };
+
 const dibujar = () => {
-    const contexto = canvas.value.getContext("2d");
     let y = 0, x = 0;
     for (const fila of arregloDeCaracter) {
         x = 0;
         for (const bit of fila) {
-            contexto.beginPath();
-            // Indicamos el color que usaremos
+            contexto?.beginPath();
             if (bit === BIT_ENCENDIDO) {
-                contexto.fillStyle = "black"
+                contexto.fillStyle = COLOR_NEGRO;
             } else {
-                contexto.fillStyle = "white"
+                contexto.fillStyle = COLOR_BLANCO;
             }
             contexto.rect(x, y, MEDIDA_CUADRO, MEDIDA_CUADRO);
             if (bit === BIT_ENCENDIDO) {
-                contexto.strokeStyle = "black";
+                contexto.strokeStyle = COLOR_NEGRO;
                 contexto.fill();
             } else {
-                contexto.strokeStyle = "#e2e8f0";
+                contexto.strokeStyle = COLOR_GRIS;
                 contexto.stroke();
                 contexto.fill();
             }
-
             contexto.closePath();
             x += MEDIDA_CUADRO;
         }
@@ -96,47 +105,7 @@ const dibujar = () => {
     }
     matriz.value = convertirArregloACadenaParaCaracter(arregloDeCaracter);
 };
-onMounted(() => {
-    canvas.value.width = arregloDeCaracter[0].length * MEDIDA_CUADRO;
-    canvas.value.height = arregloDeCaracter.length * MEDIDA_CUADRO;
-    ["mousedown", "touchstart"].forEach(nombreEvento => {
-        canvas.value.addEventListener(nombreEvento, () => {
-            isDrawing = true;
-        });
-    });
 
-    ["mouseup", "touchend"].forEach(nombreEvento => {
-        canvas.value.addEventListener(nombreEvento, () => {
-            isDrawing = false;
-        });
-    });
-    ["mousemove", "click"].forEach(nombreEvento => {
-        canvas.value.addEventListener(nombreEvento, (evento) => {
-            evento.preventDefault();
-            //evento.preventDefault();
-            if (!isDrawing && evento.type !== "click") {
-                return;
-            }
-            const [x, y] = getCursorPosition(canvas, evento);
-            const indiceX = Math.floor(x / MEDIDA_CUADRO), indiceY = Math.floor(y / MEDIDA_CUADRO);
-            if (indiceX < 0 || indiceY < 0) {
-                return;
-            }
-            if (indiceX > arregloDeCaracter[0].length - 1 || indiceY > arregloDeCaracter.length - 1) {
-                return;
-            }
-
-            if (accionSeleccionada.value === Accion.Pintar) {
-                arregloDeCaracter[indiceY][indiceX] = BIT_ENCENDIDO;
-            } else if (accionSeleccionada.value === Accion.Borrar) {
-                arregloDeCaracter[indiceY][indiceX] = BIT_APAGADO;
-            }
-            dibujar();
-        });
-    });
-    dibujar();
-
-})
 const obtenerClases = (boton: Boton) => {
     if (boton.seleccionado) {
         return boton.clasesSeleccionado;
@@ -155,6 +124,54 @@ const onClickEnBoton = (boton: Boton) => {
     boton.seleccionado = true;
     accionSeleccionada.value = boton.accion;
 }
+
+onMounted(() => {
+    verdaderoCanvas = canvas.value as HTMLCanvasElement;
+    contexto = verdaderoCanvas.getContext("2d") as CanvasRenderingContext2D;
+    limpiarCanvas();
+    verdaderoCanvas.width = arregloDeCaracter[0].length * MEDIDA_CUADRO;
+    verdaderoCanvas.height = arregloDeCaracter.length * MEDIDA_CUADRO;
+    ["mousedown", "touchstart"].forEach(nombreEvento => {
+        verdaderoCanvas.addEventListener(nombreEvento, () => {
+            isDrawing = true;
+        });
+    });
+    ["mouseup", "touchend"].forEach(nombreEvento => {
+        verdaderoCanvas.addEventListener(nombreEvento, () => {
+            isDrawing = false;
+        });
+    });
+    ["mousemove", "click", "touchmove"].forEach(nombreEvento => {
+        verdaderoCanvas.addEventListener(nombreEvento, (evento) => {
+            evento.preventDefault();
+            if (!isDrawing && evento.type !== "click") {
+                return;
+            }
+            let [x, y] = [0, 0];
+            if (evento instanceof TouchEvent) {
+                [x, y] = getCursorPosition(evento as TouchEvent);
+            } else if (evento instanceof PointerEvent) {
+                [x, y] = getCursorPosition(evento as PointerEvent);
+            } else if (evento instanceof MouseEvent) {
+                [x, y] = getCursorPosition(evento as MouseEvent);
+            }
+            const indiceX = Math.floor(x / MEDIDA_CUADRO), indiceY = Math.floor(y / MEDIDA_CUADRO);
+            if (indiceX < 0 || indiceY < 0) {
+                return;
+            }
+            if (indiceX > arregloDeCaracter[0].length - 1 || indiceY > arregloDeCaracter.length - 1) {
+                return;
+            }
+            if (accionSeleccionada.value === Accion.Pintar) {
+                arregloDeCaracter[indiceY][indiceX] = BIT_ENCENDIDO;
+            } else if (accionSeleccionada.value === Accion.Borrar) {
+                arregloDeCaracter[indiceY][indiceX] = BIT_APAGADO;
+            }
+            dibujar();
+        });
+    });
+    dibujar();
+})
 </script>
 
 <template>
