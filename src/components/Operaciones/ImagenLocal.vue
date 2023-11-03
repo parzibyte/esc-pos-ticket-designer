0e51538e0df194a0de22ff06d3a84480dad484f7
Luis Cabrera Benito <parzibyte@gmail.com>
2023-11-03 12:25:29 -0600

    Update ImagenLocal.vue

--- src/components/Operaciones/ImagenLocal.vue
+++ src/components/Operaciones/ImagenLocal.vue
@@ -2,8 +2,9 @@
 import { Alineacion, TamañoImagen, type ArgumentosParaDefinirImagenLocal } from '@/types/Tipos';
 import { computed, ref, } from 'vue';
 import CustomInput from '../CustomInput.vue';
-import Select from '../Select.vue';
 import Range from '../Range.vue';
+import SelectTamanioImagen from '../Selects/SelectTamanioImagen.vue';
+import SelectAlineacion from '../Selects/SelectAlineacion.vue';
 
 type Propiedades = {
 	modelValue: ArgumentosParaDefinirImagenLocal;
@@ -25,42 +26,6 @@ const propiedades = withDefaults(defineProps<Propiedades>(), {
 	}
 })
 
-const alineaciones = ref([
-	{
-		nombre: "Izquierda",
-		valor: Alineacion.Izquierda,
-	},
-	{
-		nombre: "Centro",
-		valor: Alineacion.Centro,
-	},
-	{
-		nombre: "Derecha",
-		valor: Alineacion.Derecha,
-	},
-
-]);
-
-
-const tamaños = ref([
-	{
-		nombre: "Normal",
-		valor: TamañoImagen.Normal,
-	},
-	{
-		nombre: "Doble ancho",
-		valor: TamañoImagen.DobleAncho,
-	},
-	{
-		nombre: "Doble largo",
-		valor: TamañoImagen.DobleLargo,
-	},
-	{
-		nombre: "Doble ancho y largo",
-		valor: TamañoImagen.DobleAnchoYLargo,
-	},
-
-]);
 const emit = defineEmits(["update:modelValue"]);
 const valorSerializado = computed({
 	set(value) {
@@ -72,18 +37,8 @@ const valorSerializado = computed({
 });
 </script>
 <template>
-	<Select :display-item-function="(tamaño) => tamaño.nombre" :items="tamaños" label="Redimensionar al imprimir:"
-		v-model="propiedades.modelValue.tamaño">
-		<template #item="{ item, index }">
-			<h1 class="text-xl">{{ item.nombre }}</h1>
-		</template>
-	</Select>
-	<Select :display-item-function="(alineacion) => alineacion.nombre" :items="alineaciones" label="Alineación"
-		v-model="propiedades.modelValue.alineacion">
-		<template #item="{ item, index }">
-			<h1 class="text-xl">{{ item.nombre }}</h1>
-		</template>
-	</Select>
+	<SelectTamanioImagen v-model="propiedades.modelValue.tamaño"></SelectTamanioImagen>
+	<SelectAlineacion v-model="propiedades.modelValue.alineacion"></SelectAlineacion>
 	<Range label="Ancho" step="8" min="8" max="648" v-model="propiedades.modelValue.maximoAncho"></Range>
 	<CustomInput type="text" placeholder="/home/parzibyte/mj.png" label="Ruta absoluta de la imagen"
 		v-model="propiedades.modelValue.ruta"></CustomInput>
