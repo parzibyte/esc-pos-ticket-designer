1028361c949be828326c50c82ce1d769d7259563
Luis Cabrera Benito <parzibyte@gmail.com>
2023-11-11 23:34:37 -0600

    Arreglando ping que siempre mostraba verde
    Hay que fijarse en "detalles" ya que esta variable estará en false cuando no haya conexión

--- src/components/Ping.vue
+++ src/components/Ping.vue
@@ -22,7 +22,7 @@ const estaConectado = computed(() => {
     if (!estadoDePlataforma) {
         return false;
     }
-    return estadoDePlataforma;
+    return estadoDePlataforma.detalles;
 });
 
 const navegarAAyuda = () => {
@@ -38,8 +38,8 @@ const navegarAAyuda = () => {
                 <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                 <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
             </span>
-            <p class="text-green-600 ml-1">{{ estaConectado.detalles.plataforma }} {{ estaConectado.detalles.version }} ({{
-                estaConectado.detalles.sistemaOperativo }})</p>
+            <p class="text-green-600 ml-1">{{ estaConectado.plataforma }} {{ estaConectado.version }} ({{
+                estaConectado.sistemaOperativo }})</p>
         </div>
     </div>
     <div class="inline-block" v-if="!estaConectado">
