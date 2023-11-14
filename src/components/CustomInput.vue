1d1d039886fa4fc3822cd19695b75b2d07666359
Luis Cabrera Benito <parzibyte@gmail.com>
2023-11-13 20:21:31 -0600

    Tamaño de texto en custom input igual al del select

--- src/components/CustomInput.vue
+++ src/components/CustomInput.vue
@@ -21,7 +21,7 @@ const value = computed({
 <template>
     <div class="flex flex-col p-1">
         <label class="inline-block font-semibold mb-1 text-md">{{ propiedades.label }}</label>
-        <input class="max-w-full w-full max-h-10 h-10 border border-gray-200 focus:border-blue-500 rounded-md p-1 outline-none focus:border-2"
+        <input class="max-w-full w-full max-h-10 h-10 border border-gray-200 focus:border-blue-500 rounded-md p-1 outline-none focus:border-2 text-xl"
             v-bind="$attrs" v-model="value">
 
     </div>
