47c9db5e7579c9d9452124e385d4422f708af0f3
Luis Cabrera Benito <parzibyte@gmail.com>
2023-11-13 20:25:18 -0600

    Separando algo de lógica en stores

---
+++ src/stores/designsStore.ts
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia'
+import { useDatabaseStore } from "@/stores/db"
+
+export const useDesignsStore = defineStore('designsStore', () => {
+	const dbStore = useDatabaseStore();
+	const insertarDiseño = async (idPlataforma: number | string, nombre: string, impresora: string) => {
+		const ahora = new Date().getTime();
+		await dbStore.exec(`INSERT INTO diseños
+		(id_plataforma, nombre, fecha_creacion, fecha_modificacion, impresora)
+		VALUES
+		(?, ?, ?, ?, ?)`,
+			[idPlataforma, nombre, ahora, ahora, impresora],
+		);
+	}
+	const actualizarDiseño = async (idPlataforma: number | string, nombre: string, impresora: string, id: number | string) => {
+		await dbStore.exec(`UPDATE diseños SET 
+		id_plataforma = ?,
+		nombre = ?,
+		fecha_modificacion = ?,
+		impresora = ?
+		WHERE id = ?  `,
+			[idPlataforma, nombre, new Date().getTime(), impresora, id],
+		);
+	}
+
+	const obtenerDiseñoPorId = async (id: number | string) => {
+		const diseñosCoincidentesConId = await dbStore.exec(`select d.id,
+	d.nombre,
+	d.fecha_creacion,
+	d.fecha_modificacion,
+	d.impresora,
+	p.id AS id_plataforma,
+	p.nombre AS plataforma,
+	p.licencia,
+	p.ruta_api
+from diseños d
+	inner join plataformas p on d.id_plataforma = p.id
+  WHERE d.id = ?`, [id]);
+		return diseñosCoincidentesConId[0];
+	}
+
+	return { obtenerDiseñoPorId, actualizarDiseño, insertarDiseño };
+});
---
+++ src/stores/platform.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+import { useDatabaseStore } from "@/stores/db"
+
+export const usePlatformStore = defineStore('platformStore', () => {
+	const dbStore = useDatabaseStore();
+	const obtenerPlataformas = async () => {
+		return await dbStore.exec("SELECT id, nombre, descripcion, ruta_api, licencia FROM plataformas");
+	}
+	const actualizarPlataforma = async (plataforma) => {
+		return await dbStore.exec(
+			"UPDATE plataformas SET ruta_api = ?, licencia = ? WHERE id = ?",
+			[plataforma.ruta_api, plataforma.licencia, plataforma.id]
+		);
+	}
+	const obtenerPlataformaPorId = async (id: string) => {
+		const plataformasCoincidentes = await dbStore.exec("SELECT id, nombre, descripcion, ruta_api, licencia FROM plataformas WHERE id = ?", [id]);
+		return plataformasCoincidentes[0];
+	}
+	return { obtenerPlataformas, actualizarPlataforma, obtenerPlataformaPorId };
+});
