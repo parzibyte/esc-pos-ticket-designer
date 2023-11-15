d86d9534d8bb81316496d910a82f4c4e2b2f2e2d
Luis Cabrera Benito <parzibyte@gmail.com>
2023-11-14 20:28:22 -0600

    Usando store correcta en el botón de imprimir

--- src/components/BotonImprimir.vue
+++ src/components/BotonImprimir.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import type { Diseño } from '@/types/Tipos';
-import { useDatabaseStore } from "@/stores/db"
 import { convertirOperacionesSerializadasAReactivas, obtenerPayloadComoJson } from '@/Helpers';
 import Printer from "vue-material-design-icons/Printer.vue";
 import { emit } from 'process';
-const store = useDatabaseStore();
+import { useDesignsOperationStore } from '@/stores/designOperation';
+const designsOperationStore = useDesignsOperationStore();
 const props = withDefaults(defineProps<{
     diseño: Diseño,
 }>(), {
@@ -20,7 +20,7 @@ const props = withDefaults(defineProps<{
 const emit = defineEmits(["error", "exito"]);
 
 const imprimir = async () => {
-    const operacionesRecuperadasDeBaseDeDatos = await store.obtenerOperacionesDeDiseño(props.diseño.id);
+    const operacionesRecuperadasDeBaseDeDatos = await designsOperationStore.obtenerOperacionesDeDiseño(props.diseño.id);
     const payload = obtenerPayloadComoJson(
         props.diseño.plataforma,
         convertirOperacionesSerializadasAReactivas(operacionesRecuperadasDeBaseDeDatos),
