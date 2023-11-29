415e0f294a6ed6009c29f9b427728c4385049943
Luis Cabrera Benito <parzibyte@gmail.com>
2023-11-29 12:10:05 -0600

    Usar componentes personalizados en todos los lugares

--- src/components/Ajustes/AjustePlataforma.vue
+++ src/components/Ajustes/AjustePlataforma.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import CustomInput from '../CustomInput.vue';
+import CustomTextarea from '../CustomTextarea.vue';
+
 const props = defineProps(["plataforma"]);
 const emit = defineEmits(["guardar"]);
 
@@ -9,16 +12,12 @@ const guardar = () => {
 <template>
     <div class="bg-white border p-2">
         <h1 class="text-3xl">{{ plataforma.nombre }}</h1>
-        <label class="block text-lg">Ruta api</label>
+        <CustomInput v-model="plataforma.ruta_api" type="text" placeholder="URL API del plugin" label="Ruta API">
+        </CustomInput>
         <small class="block">Tenga cuidado al modificar la ruta api. Si no sabe lo que está haciendo, no modifique el
             valor</small>
-        <input v-model="plataforma.ruta_api" type="text" placeholder="URL donde la API del plugin"
-            class="border border-gray-200 focus:border-blue-500 focus:border-2 rounded-md outline-none p-1 text-xl">
-        <label class="block text-lg">Serial (opcional)</label>
-        <textarea placeholder="Aquí va el serial, sin espacios ni comillas"
-            class="w-full block border border-gray-200 focus:border-blue-500 focus:border-2 rounded-md outline-none p-1 text-xl"
-            rows="5" v-model="plataforma.licencia"></textarea>
-        <br>
+        <CustomTextarea v-model="plataforma.licencia" placeholder="Serial sin espacios ni comillas" rows="5"
+            label="Serial (Opcional)"></CustomTextarea>
         <button @click="guardar"
             class="mt-2 bg-blue-500 text-white font-semibold rounded-md p-2 hover:bg-blue-400">Guardar</button>
     </div>
